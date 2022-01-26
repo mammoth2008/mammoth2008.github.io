19d61d59a553356f6902f27bc0de2fbe76b21c4e
mammoth2008 <mengma@vip.163.com>
2022-01-27 01:19:14 +0800

    rebuilding site Thu Jan 27 01:19:14 CST 2022

--- slides/slidesdeploy.sh
+++ slides/slidesdeploy.sh
@@ -3,24 +3,39 @@
 # 任一步骤执行失败都会终止整个部署过程
 set -e
 
-printf "\033[0;32mDeploying SLIDES to GitHub...\033[0m\n"
+printf "Deploying SLIDES to GitHub..."
 
-cd $HOME/Documents/1_writing/3_blog/mammoth2008.github.io
+cd $HOME/Documents/2_hust/slides
 
 # 复制除了.git/之外的文件和文件夹
 cp -Rpf `ls $HOME/Documents/2_hust/slides |grep -v .git|xargs` $HOME/Documents/1_writing/3_blog/mammoth2008.github.io/public/slides
 
-cd public
+cd $HOME/Documents/1_writing/3_blog/mammoth2008.github.io/public
 
 git add -A
 
 msg="rebuilding site $(date)"
+
 if [ -n "$*" ]; then
-msg="$*"
+	msg="$*"
 fi
 
 git commit -m "$msg"
 
 git push origin master
+
+cd $HOME/Documents/2_hust/slides
+
+git add -A
+
+msg="rebuilding site $(date)"
+
+if [ -n "$*" ]; then
+	msg="$*"
+fi
+
+git commit -m "$msg"
+
+git push origin main
 
 printf "\n\n👻👻👻\nDEPLOY SLIDES\nALL DONE\n😸😸😸\n\n"
