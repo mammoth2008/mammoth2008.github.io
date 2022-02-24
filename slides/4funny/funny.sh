e7ae1e12fe82674dd452e0e4788a01c0cc7cc9f6
mammoth2008 <mengma@vip.163.com>
2022-02-24 17:57:22 +0800

    rebuild SLIDES Thu Feb 24 17:57:22 CST 2022

---
+++ slides/4funny/funny.sh
@@ -0,0 +1,11 @@
+function rand(){
+    min=$1
+    max=$(($2 - $min + 1))
+    num=$(($RANDOM+1000000000)) # 增加一个10位的数再求余
+    echo $(($num%$max + $min))
+}
+
+# 将最后一个数字改成题目数量
+rand_no=$(rand 1 10)
+
+open "/Users/Freeman/Documents/college/slides/4funny/funny$rand_no.html"
